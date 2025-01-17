2ab23f5595bb0d68828166852f97b02ed8fdc77d
Rodrigo Borges <borgesenfermagem@gmail.com>
2025-01-16 22:17:49 -0300

    feat (Exercicio5): criado prototipo de validacao de senha

---
+++ Algoritmos/Exercicio5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio5 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            short senha = 2002;
+            short senhaUsuario = 0;
+
+            senhaUsuario = short.Parse(Console.ReadLine());
+
+            while (senha != senhaUsuario) {
+
+                Console.WriteLine("Senha Invalida");
+                senhaUsuario = short.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
