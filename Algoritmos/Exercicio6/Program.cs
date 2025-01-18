fd28a4a20df1d78f7195f2e9b84b12d6676630a2
Rodrigo Borges <borgesenfermagem@gmail.com>
2025-01-18 15:57:16 -0300

    feat(Exercicio6) adicionado verificacao de quantos numeros estao contido em um intervalo:

---
+++ Algoritmos/Exercicio6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*
+   
+Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida. 
+Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando 
+essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo). 
+ 
+ */
+
+namespace Exercicio6 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            byte N = byte.Parse(Console.ReadLine());
+            byte In = 0;
+            byte Out = 0;
+
+
+            while (N >= 1) {
+
+                N--;
+                sbyte X = sbyte.Parse(Console.ReadLine());
+
+                if (X >= 10 && X <= 20) {
+
+                    In++;
+                }
+                else {
+                    Out++;
+                }    
+
+            }
+
+            Console.WriteLine($"{In} in");
+            Console.WriteLine($"{Out} out");
+
+        }//fim main
+    }
+}
